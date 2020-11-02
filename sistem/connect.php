58fe1fd2f1dfafbeb0cf861b263e0f7c1479bac8
nureddincuneyd <nureddincuneyd@gmail.com>
2020-11-02 14:34:28 +0300

    Update connect.php

--- sistem/connect.php
+++ sistem/connect.php
@@ -1,4 +1,5 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,8 +8,9 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=ttcrm", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  //echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
